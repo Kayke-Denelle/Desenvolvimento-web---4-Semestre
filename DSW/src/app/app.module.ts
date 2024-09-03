24cec99d421a52de1588adb7b76527980eb344c1
Kayke Denelle <kaykedenelle@gmail.com>
2024-09-03 18:59:18 -0300

    att

--- DSW/src/app/app.module.ts
+++ DSW/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { TesteComponent } from './teste/teste.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    TesteComponent
   ],
   imports: [
     BrowserModule,
