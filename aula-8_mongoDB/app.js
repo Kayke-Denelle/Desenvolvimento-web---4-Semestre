793cb799c6171486466f4bfbc6a13388a18a66c5
Kayke Denelle <kaykedenelle@gmail.com>
2024-10-29 21:20:45 -0300

    aula-8

---
+++ aula-8_mongoDB/app.js
@@ -0,0 +1,33 @@
+//=====================================//
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
+
+
+//=====================================//
+const usuarioController = require('./controllers/usuarioController');
+app.use('/usuarios', usuarioController);
+const postController = require('./controllers/postController');
+app.use('/posts', postController);
+//=====================================//
+
+//=====================================//
+mongoose.connect('mongodb://127.0.0.1:27017/aula10')
+.then(() => {
+app.listen(27017, () => {
+console.log('Conectado ao mongoDB');
+console.log('Servidor iniciado na porta 27017');
+})
+})
+.catch((err) => {
+console.log(err);
+});
+
+
+//=====================================//
+
+//=====================================//
+app.listen(3000, () => console.log('Servidor rodando na porta: 3000'));
+
+//=====================================//
